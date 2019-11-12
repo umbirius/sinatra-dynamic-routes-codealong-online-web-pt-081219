63077c840a19826b9b99d19666d8df9d73c5add3
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-12 17:01:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,8 +22,7 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    <%prod = @num1 * @num2%>
-    "#{prod}"
+    "#{@num1*@num2}"
   end 
 
 end
